89b357a98cf3c616faec6a7996250e8b11891038
faracetamol <farahdivanazwanda@gmail.com>
2026-04-28 19:04:44 +0700

    update project concertix

---
+++ resources/views/tentang.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Tentang</title>
+</head>
+<body>
+
+<h1>Tentang Kami</h1>
+<p>Ini adalah halaman tentang aplikasi Concertix.</p>
+
+</body>
+</html>
---
+++ routes/web.php
@@ -0,0 +1,16 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\DashboardController;
+
+Route::get('/', [DashboardController::class, 'index'])->name('dashboard');
+Route::get('/tentang', function () {
+    return view('tentang');
+});
+Route::get('/hitung/{a}/{b}', function ($a, $b) {
+    return $a + $b;
+});
+
+Route::post('/tambah', [DashboardController::class, 'store']);
+Route::post('/update/{id}', [DashboardController::class, 'update']);
+Route::get('/hapus/{id}', [DashboardController::class, 'delete']);

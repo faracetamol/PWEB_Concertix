5300422994b96765f46a79574bd91f39e876d10f
faracetamol <farahdivanazwanda@gmail.com>
2026-06-12 22:03:23 +0700

    fix

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,25 +2,16 @@
 
 namespace Database\Seeders;
 
-use App\Models\User;
-use App\Models\Event;
-use App\Models\Ticket;
 use Illuminate\Database\Seeder;
 
-class TicketSeeder extends Seeder
+class DatabaseSeeder extends Seeder
 {
     public function run(): void
     {
-        $user = User::first();
-        $event = Event::first();
-
-        if ($user && $event) {
-            Ticket::create([
-                'user_id' => $user->id,
-                'event_id' => $event->id,
-                'jumlah_tiket' => 2,
-                'total_harga' => 300000,
-            ]);
-        }
+        $this->call([
+            UserSeeder::class,
+            EventSeeder::class,
+            TicketSeeder::class,
+        ]);
     }
 }
